4f1f4630e0e328b85b8f849037570e73a9cc43ce
linzell <linzellart@gmail.com>
2024-10-26 15:31:09 +0200

    fix(host): Add env

--- host/module-federation.config.ts
+++ host/module-federation.config.ts
@@ -2,8 +2,8 @@ import { createModuleFederationConfig } from '@module-federation/modern-js';
 export default createModuleFederationConfig({
   name: 'host',
   remotes: {
-    remote: 'remote@http://localhost:3051/mf-manifest.json',
-    nested_remote: 'nested_remote@http://localhost:3052/mf-manifest.json',
+    remote: `remote@${process.env.REMOTE_URL || 'http://localhost:3051'}/mf-manifest.json`,
+    nested_remote: `nested_remote@${process.env.NESTED_REMOTE_URL || 'http://localhost:3052'}/mf-manifest.json`,
   },
   shared: {
     react: { singleton: true },
